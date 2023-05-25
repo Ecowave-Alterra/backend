00d193d25b53c582bc061befa7406194d88faa48
berrylradianh <berrylhamesha@gmail.com>
2023-05-26 00:05:52 +0700

    fixing cloudbuild

--- config/config.go
+++ config/config.go
@@ -13,11 +13,11 @@ type Configuration struct {
 func GetConfig() Configuration {
 
 	configuration := Configuration{
-		DB_USERNAME: os.Getenv("_DB_USERNAME"),
-		DB_PASSWORD: os.Getenv("_DB_PASSWORD"),
-		DB_PORT:     os.Getenv("_DB_PORT"),
-		DB_HOST:     os.Getenv("_DB_HOST"),
-		DB_NAME:     os.Getenv("_DB_NAME"),
+		DB_USERNAME: os.Getenv("DB_USERNAME"),
+		DB_PASSWORD: os.Getenv("DB_PASSWORD"),
+		DB_PORT:     os.Getenv("DB_PORT"),
+		DB_HOST:     os.Getenv("DB_HOST"),
+		DB_NAME:     os.Getenv("DB_NAME"),
 	}
 
 	return configuration
