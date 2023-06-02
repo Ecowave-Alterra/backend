46494ea5ff42082a7eb0e1010fc67875534ae6cb
berrylradianh <berrylhamesha@gmail.com>
2023-06-02 22:08:17 +0700

    fixing cors failed

--- cmd/routes/route.go
+++ cmd/routes/route.go
@@ -3,15 +3,15 @@ package routes
 import (
 	"github.com/berrylradianh/ecowave-go/common"
 	vld "github.com/berrylradianh/ecowave-go/helper/validator"
-	"github.com/berrylradianh/ecowave-go/middleware/log"
 	"github.com/go-playground/validator"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func StartRoute(handler common.Handler) *echo.Echo {
 	e := echo.New()
 	e.Validator = &vld.CustomValidator{Validator: validator.New()}
-	log.LogMiddleware(e)
+	e.Use(middleware.CORS())
 
 	handler.AuthHandler.RegisterRoutes(e)
 	handler.InformationHandler.RegisterRoutes(e)
