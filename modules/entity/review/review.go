f0b6f172e9878a4aab298b6f464c9dd8c5ffc4b7
NadiraAdiva257 <adivanadira257@gmail.com>
2023-06-16 11:35:54 +0700

    edit entity review

--- modules/entity/review/review.go
+++ modules/entity/review/review.go
@@ -14,22 +14,3 @@ type RatingProduct struct {
 	VideoUrl            string  `json:"VideoUrl" form:"VideoUrl"`
 	TransactionDetailId uint    `json:"TransactionDetailId" form:"TransactionDetailId"`
 }
-
-type Review struct {
-	*gorm.Model
-
-	RatingService float64        `json:"RatingService" form:"RatingService"`
-	TransactionId uint           `json:"TransactionId" form:"TransactionId"`
-	ReviewDetails []ReviewDetail `gorm:"foreignKey:ReviewId"`
-}
-
-type ReviewDetail struct {
-	*gorm.Model
-
-	RatingProduct float64 `json:"RatingProduct" form:"RatingProduct"`
-	Comment       string  `json:"Comment" form:"Comment"`
-	CommentAdmin  string  `json:"CommentAdmin" form:"CommentAdmin"`
-	PhotoUrl      string  `json:"PhotoUrl" form:"PhotoUrl"`
-	VideoUrl      string  `json:"VideoUrl" form:"VideoUrl"`
-	ReviewId      uint    `json:"ReviewId" form:"ReviewId"`
-}
