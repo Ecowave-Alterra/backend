d7cfe3928dba45fe6769e89f322e6a496ae01a92
NadiraAdiva257 <adivanadira257@gmail.com>
2023-06-19 13:17:00 +0700

    edit user seed

--- database/seed/user_detail_seed.go
+++ database/seed/user_detail_seed.go
@@ -7,18 +7,18 @@ import (
 func CreateUserDetail() []*ut.UserDetail {
 	userDetail := []*ut.UserDetail{
 		{
-			Name:            "user1 fullname",
+			Name:            "User 1",
 			Point:           0,
-			Phone:           "08998754321",
-			ProfilePhotoUrl: "https://storage.cloud.google.com/ecowave/img/users/profile/profile.png",
-			UserId:          1,
+			Phone:           "08917283129283",
+			ProfilePhotoUrl: "https://storage.googleapis.com/ecowave/img/users/profile/profile.png",
+			UserId:          2,
 		},
 		{
-			Name:            "user2 fullname",
+			Name:            "User 2",
 			Point:           0,
-			Phone:           "08998754321",
-			ProfilePhotoUrl: "https://storage.cloud.google.com/ecowave/img/users/profile/profile.png",
-			UserId:          2,
+			Phone:           "0851728392716",
+			ProfilePhotoUrl: "https://storage.googleapis.com/ecowave/img/users/profile/profile.png",
+			UserId:          3,
 		},
 	}
 
--- database/seed/user_seed.go
+++ database/seed/user_seed.go
@@ -8,21 +8,26 @@ import (
 func CreateUser() []*ut.User {
 	hashPasswordUser1, _ := password.HashPassword("user1")
 	hashPasswordUser2, _ := password.HashPassword("user2")
+	hashPasswordAdmin, _ := password.HashPassword("admin123")
 
 	user := []*ut.User{
 		{
-			RoleId:   2,
-			Email:    "user1@gmail.com",
-			GoogleId: "qwertyuiop",
-			Username: "user1",
-			Password: string(hashPasswordUser1),
+			Email:    "admin@gmail.com",
+			Username: "admin",
+			Password: string(hashPasswordAdmin),
+			RoleId:   1,
 		},
 		{
+			Email:    "user1@gmail.com",
+			Username: "user1",
+			Password: string(hashPasswordUser1),
 			RoleId:   2,
+		},
+		{
 			Email:    "user2@gmail.com",
-			GoogleId: "asdfghjkl",
 			Username: "user2",
 			Password: string(hashPasswordUser2),
+			RoleId:   2,
 		},
 	}
 
