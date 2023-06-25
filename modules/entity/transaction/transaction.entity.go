781620a67dd85b6a4aba9479a97525bf798d18c3
berrylradianh <berrylhamesha@gmail.com>
2023-06-25 11:54:15 +0700

    fixing repo

--- modules/entity/transaction/transaction.entity.go
+++ modules/entity/transaction/transaction.entity.go
@@ -1,6 +1,8 @@
 package transaction
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
@@ -26,4 +28,40 @@ type Transaction struct {
 	Discount           float64             `json:"Discount" form:"Discount"`
 	TotalPrice         float64             `validate:"required"`
 	TransactionDetails []TransactionDetail `json:"TransactionDetails" form:"TransactionDetails" gorm:"foreignKey:TransactionId"`
+}
+
+type TransactionResponse struct {
+	ReceiptNumber     string
+	TransactionId     string
+	Name              string
+	Unit              uint
+	TotalPrice        float64
+	OrderDate         time.Time
+	StatusTransaction string
+}
+
+type TransactionDetailResponse struct {
+	Address            string
+	Voucher            string
+	Name               string
+	PhoneNumber        string
+	ReceiptNumber      string
+	TotalProductPrice  float64
+	TotalShippingPrice float64
+	TotalPrice         float64
+	Point              float64
+	PaymentMethod      string
+	PaymentStatus      string
+	CanceledReason     string
+	ExpeditionRating   float32
+	ExpeditionName     string
+	StatusTransaction  string
+	CreatedAt          time.Time
+	UpdatedAt          time.Time
+}
+
+type TransactionProductDetailResponse struct {
+	ProductName     string
+	ProductImageUrl string
+	Qty             string
 }
